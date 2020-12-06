bb590ed868027048a519fa5cf854323a3adcd833
Mariam <mariam.manvelidze.1@btu.edu.ge>
2020-12-06 16:24:11 +0400

    feat: current & specific map, units

--- js/main.js
+++ js/main.js
@@ -26,17 +26,32 @@ let forecastThirdImg = document.getElementById("forecast-day-3_img");
 let searchInput = document.getElementById("header-search-input");
 let searchButton = document.getElementById("header-search-button");
 
-function getWeather(cityName) {
-  let weatherAPI = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&lang=en&units=metric&APPID=d947bf1a0211edc7f91dcb84156c547a`;
+// latitude, longitude, map
+let latitude = document.getElementById("latitude");
+let longitude = document.getElementById("longitude");
+let map = document.getElementById("map");
+
+// units
+let fahrUnit = document.getElementById("temperature-units_fahr");
+let celsUnit = document.getElementById("temperature-units_cels");
+
+// get weather info from api & assign
+function getWeather(cityName, lang, unit) {
+  let weatherAPI = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&lang=${lang}&units=${unit}&APPID=d947bf1a0211edc7f91dcb84156c547a`;
   fetch(weatherAPI)
     .then((res) => res.json())
     .then((data) => {
+      // lat & long
+      let lat = data.city.coord.lat;
+      let long = data.city.coord.lon;
+      getMap(lat, long);
       // city
+      console.log(data);
       city.textContent = `${cityName}, ${data.city.country}`;
 
       // assign current weather info
       currentTemp.textContent = data.list[1].main.temp.toFixed(0);
-      weatherDesc.textContent = data.list[1].weather[0].main;
+      weatherDesc.textContent = data.list[1].weather[0].description;
       weatherFeels.textContent = data.list[1].main.feels_like.toFixed(0);
       windSpd.textContent = data.list[1].wind.speed.toFixed(0);
       humidity.textContent = data.list[1].main.humidity;
@@ -48,6 +63,24 @@ function getWeather(cityName) {
     });
 }
 
+function assignLatLong(lat, long) {
+  latitude.textContent = `${lat.toFixed(2)}'`.replace(".", "°");
+  longitude.textContent = `${long.toFixed(2)}'`.replace(".", "°");
+}
+
+function getMap(lat, long) {
+  mapboxgl.accessToken =
+    "pk.eyJ1IjoibWFyaWFtbWFudmVsaWR6ZSIsImEiOiJja2lkMWV3djQwNTRvMnVyd3NibGl1bWhoIn0.6J6Vnfk6deSuWUapxzAfSQ";
+  var map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: [long, lat],
+    zoom: 8,
+  });
+
+  new mapboxgl.Marker().setLngLat([long, lat]).addTo(map);
+}
+
 window.addEventListener("load", () => {
   let long;
   let lat;
@@ -57,6 +90,11 @@ window.addEventListener("load", () => {
     navigator.geolocation.getCurrentPosition((position) => {
       long = position.coords.longitude;
       lat = position.coords.latitude;
+
+      // assing latitude & longitude
+      assignLatLong(lat, long);
+      // display map
+      getMap(lat, long);
 
       // get current city
       const apikey = "e4f7ce76ece34f5fa88d15a1e67d9f9f";
@@ -77,14 +115,24 @@ window.addEventListener("load", () => {
         })
         .then((data) => {
           // get & assign current location weather info
-          getWeather(data.results[0].components.city);
+          getWeather(data.results[0].components.city, "en", "metric");
+          fahrUnit.addEventListener("click", () => {
+            getWeather(data.results[0].components.city, "en", "imperial");
+            fahrUnit.classList.remove("inactive");
+            celsUnit.classList.add("inactive");
+          });
+          celsUnit.addEventListener("click", () => {
+            getWeather(data.results[0].components.city, "en", "metric");
+            fahrUnit.classList.add("inactive");
+            celsUnit.classList.remove("inactive");
+          });
         });
     });
   }
 
   searchButton.addEventListener("click", () => {
     if (searchInput) {
-      getWeather(searchInput.value);
+      getWeather(searchInput.value, "en", "metric");
     } else {
       alert("Input empty or incorrect city name");
     }
