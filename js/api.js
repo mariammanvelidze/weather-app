8cc27e751c3b40fa34e84f92aaf583bf0e7df30b
Mariam <mariam.manvelidze.1@btu.edu.ge>
2020-12-14 14:49:50 +0400

    feat: eslint

--- js/api.js
+++ js/api.js
@@ -1,18 +1,17 @@
+/*eslint-disable */
 export function getMap(lat, long) {
-  mapboxgl.accessToken =
-    "pk.eyJ1IjoibWFyaWFtbWFudmVsaWR6ZSIsImEiOiJja2lkMWV3djQwNTRvMnVyd3NibGl1bWhoIn0.6J6Vnfk6deSuWUapxzAfSQ";
-  var map = new mapboxgl.Map({
-    container: "map",
-    style: "mapbox://styles/mapbox/streets-v11",
+  mapboxgl.accessToken = 'pk.eyJ1IjoibWFyaWFtbWFudmVsaWR6ZSIsImEiOiJja2lkMWV3djQwNTRvMnVyd3NibGl1bWhoIn0.6J6Vnfk6deSuWUapxzAfSQ';
+  const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
     center: [long, lat],
     zoom: 8,
   });
 }
 
 export function getMapByCity(city) {
-  mapboxgl.accessToken =
-    "pk.eyJ1IjoibWFyaWFtbWFudmVsaWR6ZSIsImEiOiJja2lkMWV3djQwNTRvMnVyd3NibGl1bWhoIn0.6J6Vnfk6deSuWUapxzAfSQ";
-  var mapboxClient = mapboxSdk({ accessToken: mapboxgl.accessToken });
+  mapboxgl.accessToken = 'pk.eyJ1IjoibWFyaWFtbWFudmVsaWR6ZSIsImEiOiJja2lkMWV3djQwNTRvMnVyd3NibGl1bWhoIn0.6J6Vnfk6deSuWUapxzAfSQ';
+  const mapboxClient = mapboxSdk({ accessToken: mapboxgl.accessToken });
   mapboxClient.geocoding
     .forwardGeocode({
       query: `${city}`,
@@ -20,18 +19,18 @@ export function getMapByCity(city) {
       limit: 1,
     })
     .send()
-    .then(function (response) {
+    .then((response) => {
       if (
-        response &&
-        response.body &&
-        response.body.features &&
-        response.body.features.length
+        response
+        && response.body
+        && response.body.features
+        && response.body.features.length
       ) {
-        var feature = response.body.features[0];
+        const feature = response.body.features[0];
 
-        var map = new mapboxgl.Map({
-          container: "map",
-          style: "mapbox://styles/mapbox/streets-v11",
+        const map = new mapboxgl.Map({
+          container: 'map',
+          style: 'mapbox://styles/mapbox/streets-v11',
           center: feature.center,
           zoom: 8,
         });
--- js/main.js
+++ js/main.js
@@ -1,54 +1,59 @@
-import { getImg, getMap, getMapByCity } from "./api.js";
+import { getImg, getMap, getMapByCity } from './api.js';
 
 // get main elemenets from html
-let city = document.getElementById("city");
-let date = document.getElementById("date");
-let time = document.getElementById("time");
-let currentTemp = document.getElementById("current-temperature");
-let weatherImg = document.getElementById("current-weather-img");
-let weatherDesc = document.getElementById("weather-desc");
-let weatherFeels = document.getElementById("weather-feels-like");
-let windSpd = document.getElementById("wind-speed");
-let humidity = document.getElementById("humidity");
+const city = document.getElementById('city');
+const date = document.getElementById('date');
+const time = document.getElementById('time');
+const currentTemp = document.getElementById('current-temperature');
+const weatherImg = document.getElementById('current-weather-img');
+const weatherDesc = document.getElementById('weather-desc');
+const weatherFeels = document.getElementById('weather-feels-like');
+const windSpd = document.getElementById('wind-speed');
+const humidity = document.getElementById('humidity');
 
 // Forecast
-let forecastFirst = document.getElementById("forecast-day-1");
-let forecastFirstTemp = document.getElementById("forecast-day-1_temp");
-let forecastFirstImg = document.getElementById("forecast-day-1_img");
+const forecastFirst = document.getElementById('forecast-day-1');
+const forecastFirstTemp = document.getElementById('forecast-day-1_temp');
+const forecastFirstImg = document.getElementById('forecast-day-1_img');
 
-let forecastSec = document.getElementById("forecast-day-2");
-let forecastSecTemp = document.getElementById("forecast-day-2_temp");
-let forecastSecImg = document.getElementById("forecast-day-2_img");
+const forecastSec = document.getElementById('forecast-day-2');
+const forecastSecTemp = document.getElementById('forecast-day-2_temp');
+const forecastSecImg = document.getElementById('forecast-day-2_img');
 
-let forecastThird = document.getElementById("forecast-day-3");
-let forecastThirdTemp = document.getElementById("forecast-day-3_temp");
-let forecastThirdImg = document.getElementById("forecast-day-3_img");
+const forecastThird = document.getElementById('forecast-day-3');
+const forecastThirdTemp = document.getElementById('forecast-day-3_temp');
+const forecastThirdImg = document.getElementById('forecast-day-3_img');
 
 // search input & button
-let searchInput = document.getElementById("header-search-input");
-let searchButton = document.getElementById("header-search-button");
+const searchInput = document.getElementById('header-search-input');
+const searchButton = document.getElementById('header-search-button');
 
 // latitude, longitude, map
-let latitude = document.getElementById("latitude");
-let longitude = document.getElementById("longitude");
-let map = document.getElementById("map");
+const latitude = document.getElementById('latitude');
+const longitude = document.getElementById('longitude');
+// const map = document.getElementById('map');
 
 // refresh bg button
-let refreshBg = document.getElementById("refresh-button");
+const refreshBg = document.getElementById('refresh-button');
 
 // units
-let fahrUnit = document.getElementById("temperature-units_fahr");
-let celsUnit = document.getElementById("temperature-units_cels");
+const fahrUnit = document.getElementById('temperature-units_fahr');
+const celsUnit = document.getElementById('temperature-units_cels');
+
+function assignLatLong(lat, long) {
+  latitude.textContent = `${lat.toFixed(2)}'`.replace('.', '°');
+  longitude.textContent = `${long.toFixed(2)}'`.replace('.', '°');
+}
 
 // get weather info from api & assign
 function getWeather(cityName, unit) {
-  let weatherAPI = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&lang=en&units=${unit}&APPID=d947bf1a0211edc7f91dcb84156c547a`;
+  const weatherAPI = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&lang=en&units=${unit}&APPID=d947bf1a0211edc7f91dcb84156c547a`;
   fetch(weatherAPI)
     .then((res) => res.json())
     .then((data) => {
       // lat & long
-      let lat = data.city.coord.lat;
-      let long = data.city.coord.lon;
+      const { lat } = data.city.coord;
+      const long = data.city.coord.lon;
       // getMap(lat, long);
       assignLatLong(lat, long);
       // city
@@ -75,18 +80,13 @@ function getWeather(cityName, unit) {
     });
 }
 
-function assignLatLong(lat, long) {
-  latitude.textContent = `${lat.toFixed(2)}'`.replace(".", "°");
-  longitude.textContent = `${long.toFixed(2)}'`.replace(".", "°");
-}
-
 // get current city & assign info
 
 if (navigator.geolocation) {
   // get current latitude & longitude
   navigator.geolocation.getCurrentPosition((position) => {
-    let long = position.coords.longitude;
-    let lat = position.coords.latitude;
+    const long = position.coords.longitude;
+    const lat = position.coords.latitude;
 
     // assing latitude & longitude
     assignLatLong(lat, long);
@@ -94,99 +94,91 @@ if (navigator.geolocation) {
     getMap(lat, long);
 
     // get current city
-    const apikey = "e4f7ce76ece34f5fa88d15a1e67d9f9f";
-    const api_url = "https://api.opencagedata.com/geocode/v1/json";
-    let url =
-      api_url +
-      "?" +
-      "key=" +
-      apikey +
-      "&q=" +
-      encodeURIComponent(lat + "," + long) +
-      "&pretty=1" +
-      "&no_annotations=1";
+    const apikey = 'e4f7ce76ece34f5fa88d15a1e67d9f9f';
+    const apiUrl = 'https://api.opencagedata.com/geocode/v1/json';
+    const url = `${apiUrl}?`
+      + `key=${apikey}&q=${encodeURIComponent(`${lat},${long}`)}&pretty=1`
+      + '&no_annotations=1';
 
     fetch(url)
-      .then((response) => {
-        return response.json();
-      })
+      .then((response) => response.json())
       .then((data) => {
         // get & assign current location weather info
         // has to change ??
-        getWeather(data.results[0].components.city, "metric");
-        fahrUnit.addEventListener("click", () => {
-          getWeather(data.results[0].components.city, "imperial");
-          fahrUnit.classList.remove("inactive");
-          celsUnit.classList.add("inactive");
+        getWeather(data.results[0].components.city, 'metric');
+        fahrUnit.addEventListener('click', () => {
+          getWeather(data.results[0].components.city, 'imperial');
+          fahrUnit.classList.remove('inactive');
+          celsUnit.classList.add('inactive');
         });
-        celsUnit.addEventListener("click", () => {
-          getWeather(data.results[0].components.city, "metric");
-          fahrUnit.classList.add("inactive");
-          celsUnit.classList.remove("inactive");
+        celsUnit.addEventListener('click', () => {
+          getWeather(data.results[0].components.city, 'metric');
+          fahrUnit.classList.add('inactive');
+          celsUnit.classList.remove('inactive');
         });
       });
   });
 }
 
+function setToCels() {
+  fahrUnit.classList.add('inactive');
+  celsUnit.classList.remove('inactive');
+}
+
 // get city name from input and display info
-searchButton.addEventListener("click", () => {
+searchButton.addEventListener('click', () => {
   getMapByCity(searchInput.value);
-  getWeather(searchInput.value, "metric");
+  getWeather(searchInput.value, 'metric');
   setToCels();
-  fahrUnit.addEventListener("click", () => {
-    getWeather(searchInput.value, "imperial");
-    fahrUnit.classList.remove("inactive");
-    celsUnit.classList.add("inactive");
+  fahrUnit.addEventListener('click', () => {
+    getWeather(searchInput.value, 'imperial');
+    fahrUnit.classList.remove('inactive');
+    celsUnit.classList.add('inactive');
   });
-  celsUnit.addEventListener("click", () => {
-    getWeather(searchInput.value, "metric");
-    fahrUnit.classList.add("inactive");
-    celsUnit.classList.remove("inactive");
+  celsUnit.addEventListener('click', () => {
+    getWeather(searchInput.value, 'metric');
+    fahrUnit.classList.add('inactive');
+    celsUnit.classList.remove('inactive');
   });
 });
 
-function setToCels() {
-  fahrUnit.classList.add("inactive");
-  celsUnit.classList.remove("inactive");
-}
-
 // display date & time
 function clockTick() {
-  let d = new Date();
-  let weekday = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
-  let fullWeekday = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
+  const d = new Date();
+  const weekday = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
+  const fullWeekday = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
   ];
 
   // get current & forecast days
-  let currentWeekday = weekday[d.getDay()];
-  let secondDay = fullWeekday[d.getDay() + 1];
-  let thirdDay = fullWeekday[d.getDay() + 2];
-  let fourthDay = fullWeekday[d.getDay() + 3];
+  const currentWeekday = weekday[d.getDay()];
+  const secondDay = fullWeekday[d.getDay() + 1];
+  const thirdDay = fullWeekday[d.getDay() + 2];
+  const fourthDay = fullWeekday[d.getDay() + 3];
 
-  let month = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
+  const month = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
   ];
 
   // assign current time
-  let currentMonth = month[d.getMonth()];
+  const currentMonth = month[d.getMonth()];
   date.textContent = `${currentWeekday} ${d.getDate()} ${currentMonth}`;
   time.textContent = `${d.getHours()}:${d.getMinutes()}`;
 
@@ -201,20 +193,20 @@ setInterval(clockTick, 1000);
 
 // gets background image according to time (day/night)
 function getImgByTime() {
-  let hours = time.textContent.split(":")[0];
+  const hours = time.textContent.split(':')[0];
   if (hours > 17) {
-    getImg("night");
+    getImg('night');
   } else {
-    getImg("morning");
+    getImg('morning');
   }
 }
 
 // change background image on refresh button click
-refreshBg.addEventListener("click", () => {
+refreshBg.addEventListener('click', () => {
   getImgByTime();
 });
 
 // change background image on page load
-window.addEventListener("load", () => {
+window.addEventListener('load', () => {
   getImgByTime();
 });
